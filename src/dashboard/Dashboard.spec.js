fc2fb65a2116c5e2d708ac87f4dfaf28faa096f8
gebhartn <nicholas.gebhart@gmail.com>
2019-10-14 13:06:30 -0700

    test: add dashboard tests

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -4,3 +4,17 @@
                 - Display
 
 */
+
+import React from 'react';
+import { render } from '@testing-library/react';
+
+import Display from '../display/Display';
+import Controls from '../controls/Controls';
+
+test('Display renders', () => {
+  render(<Display />);
+});
+
+test('Controls renders', () => {
+  render(<Controls />);
+});
